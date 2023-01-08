b92af1b7a1dfcc238905818a7be57931adc4cdf0
RagehAz <rageh.az@gmail.com>
2023-01-08 18:05:15 +0200

    - fixed importing colors

--- lib/bldrs_theme.dart
+++ lib/bldrs_theme.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: unnecessary_import
+
 library bldrs_theme;
 
 import 'package:flutter/services.dart';
+import 'package:flutter/painting.dart';
 
 part './colorz.dart';
 part './iconz.dart';
