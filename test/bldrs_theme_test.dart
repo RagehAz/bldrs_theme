de1edfd97a7f11cb7791252552e9376ebb42023d
RagehAz <rageh.az@gmail.com>
2023-01-08 13:01:16 +0200

    - added method test for Iconz.checkAssetExists()

--- test/bldrs_theme_test.dart
+++ test/bldrs_theme_test.dart
@@ -1,6 +1,74 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:bldrs_theme/bldrs_theme.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bldrs_theme/bldrs_theme.dart';
+
 
 void main() {
+// -----------------------------------------------------------------------------
+  test('checkAssetExists', () async {
 
+    const String icon = Iconz.bz;
+
+    final bool exists = await Iconz.checkAssetExists(icon);
+    expect(exists, false);
+
+    /// this method will fail to be used here
+    /// as it uses the below path, which only works when used in other
+    /// projects depending on bldrs_theme package
+    /// final String _path = 'packages/bldrs_theme/$bldrsThemeAsset';
+    /// so in order to check the method [Iconz.checkAssetExists]
+    /// an altered path is used here
+    ///
+    /// AND I GUESS rootBundle will not work as well because assets are not bundled here
+    /// and are not mentioned in pubspec.yaml
+
+    Future<bool> checkAssetExistsWithAlteredPathForTesting(String bldrsThemeAsset) async {
+      bool _exists = false;
+
+      final String _path = bldrsThemeAsset;
+
+      if (bldrsThemeAsset != null) {
+        await rootBundle.load(_path).then((_) {
+          _exists = true;
+        }).catchError((error) {
+          _exists = false;
+        });
+      }
+
+      return _exists;
+    }
+
+    final bool existsWithAlteredPath = await checkAssetExistsWithAlteredPathForTesting(icon);
+    expect(existsWithAlteredPath, false);
+
+    const String icon2 = 'lib/assets/icons/gi_play.svg';
+    final bool exists2 = await checkAssetExistsWithAlteredPathForTesting(icon2);
+    expect(exists2, false);
+
+    const String icon3 = 'lib/assets/icons/gi_bzzzzzz.jpg';
+    final bool exists3 = await checkAssetExistsWithAlteredPathForTesting(icon3);
+    expect(exists3, false);
+
+    /// so to properly test this method, bldrs_theme package should be imported
+    /// in another project
+    /// then
+    /// tested there, just copy the below method and test in another project
+
+  });
+  // -----------------------------------------------------------------------------
+  /// THIS TEST TO BE USED IN ANOTHER PROJECT AFTER IMPORTING bldrs_theme PACKAGE
+  /*
+  test('check bldrs_theme asset exists', () async {
+
+    const String icon = Iconz.bz;
+    final bool exists = await Iconz.checkAssetExists(icon);
+    expect(exists, true);
+
+    const String icon2 = 'lib/assets/icons/gi_bzzzzzz.jpg';
+    final bool exists2 = await Iconz.checkAssetExists(icon2);
+    expect(exists2, false);
+
+  });
+   */
+  // -----------------------------------------------------------------------------
 }
